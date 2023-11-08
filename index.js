a6e2d1096e44d7d6fd900295674ca6fbf9a227ee
rnzim <rnxpphp@gamil.com>
2023-11-07 21:45:45 -0300

    fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 app.use('/',router)
 
 app.listen(3000,()=>{

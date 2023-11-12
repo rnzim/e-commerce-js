a1d2809c07e0db86ab265a31ae32adc6bd7e6497
Raimundo Nonato <144294493+rnzim@users.noreply.github.com>
2023-11-12 19:53:19 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const knex = require('knex')({client:'mysql2',connection:{
-    host:'localhost',
-    port:3306,
-    password:'',
+    host:'roundhouse.proxy.rlwy.net',
+    port:26453,
+    password:'4gAfB13f5eGGHHd6A1-aha5bacdDH64H',
     user:'root',
     database:'e_commerce'
 
